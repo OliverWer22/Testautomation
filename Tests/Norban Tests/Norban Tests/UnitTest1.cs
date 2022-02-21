8f09b8375cb3272c4ea3cf8068ff9e82476602b7
Oliver W <oliver.werthen@hotmail.com>
2022-02-21 10:24:04 +0100

    Updates to tests
    Including initilize and cleanup

--- Tests/Norban Tests/Norban Tests/UnitTest1.cs
+++ Tests/Norban Tests/Norban Tests/UnitTest1.cs
@@ -10,9 +10,11 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
+
 [TestFixture]
 public class VerifyLinksTest
 {
+
     private IWebDriver driver;
     public IDictionary<string, object> vars { get; private set; }
     private IJavaScriptExecutor js;
@@ -22,44 +24,80 @@ public class VerifyLinksTest
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
-    }
-    /*public void Initialize()
-    {
         driver.Navigate().GoToUrl("https://norban.se/");
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")));
         driver.Manage().Window.Size = new System.Drawing.Size(1552, 832);
     }
-    */
-        [TearDown]
+    public void Initialize()
+    {
+        driver.Navigate().GoToUrl("https://norban.se/");
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")));
+        driver.Manage().Window.Size = new System.Drawing.Size(1552, 832);
+    }
+
+    [TearDown]
     protected void TearDown()
     {
         driver.Quit();
     }
+    
+    public void Cleanup()
+    {
+        driver.Manage().Cookies.DeleteAllCookies();
+    }
+
     [Test]
     public void verifyHomeButton()
     {
-        driver.Navigate().GoToUrl("https://norban.se/");
-        driver.Manage().Window.Size = new System.Drawing.Size(1552, 832);
         driver.FindElement(By.CssSelector(".Header-logo")).Click();
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")));
         Assert.That(driver.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")).Text, Is.EqualTo("Mänskliga mäklare. Digitala möjligheter."));
         driver.FindElement(By.LinkText("Så funkar Norban")).Click();
-        Assert.That(driver.FindElement(By.XPath("/html/body/div[1]/main/div/div/section[1]/div/div[1]/h1")).Text, Is.EqualTo("Varför stressa igenom ditt livs största affär?"));
-        driver.FindElement(By.CssSelector(".Header-logoPath")).Click();
-        Assert.That(driver.FindElement(By.XPath("//*[@id='react - root']/div/section[1]/div/div[1]/p[1]/text()[1]")).Text, Is.EqualTo("Mänskliga mäklare. Digitala möjligheter."));
+        WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult2 = wait.Until(e => e.FindElement(By.CssSelector(".SellPage-introTitle")));
+        Assert.That(driver.FindElement(By.CssSelector(".SellPage-introTitle")).Text, Is.EqualTo("Varför stressa igenom ditt livs största affär?"));
+        driver.FindElement(By.CssSelector(".Header-logo")).Click();
+        WebDriverWait wait3 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult3 = wait.Until(e => e.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")));
+        Assert.That(driver.FindElement(By.CssSelector(".FrontPage-topAboveFoldTagline")).Text, Is.EqualTo("Mänskliga mäklare. Digitala möjligheter."));
+
     }
     [Test]
     public void verifyNyproduktion()
     {
-        driver.Navigate().GoToUrl("https://norban.se/");
-        driver.Manage().Window.Size = new System.Drawing.Size(1536, 816);
         driver.FindElement(By.LinkText("Nyproduktion")).Click();
-        Assert.That(driver.FindElement(By.XPath("/html/body/div[1]/main/div/div/div/div[1]/h1")).Text, Is.EqualTo("Nyproduktion"));
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".NewConstruction-title")));
+        Assert.That(driver.FindElement(By.CssSelector(".NewConstruction-title")).Text, Is.EqualTo("Nyproduktion"));
     }
     [Test]
-    public void verifySfunkarNorban()
+    public void verifySåfunkarNorban()
     {
-        driver.Navigate().GoToUrl("https://norban.se/");
-        driver.Manage().Window.Size = new System.Drawing.Size(1536, 816);
         driver.FindElement(By.LinkText("Så funkar Norban")).Click();
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".SellPage-introTitle")));
         Assert.That(driver.FindElement(By.CssSelector(".SellPage-introTitle")).Text, Is.EqualTo("Varför stressa igenom ditt livs största affär?"));
     }
+    [Test]
+    public void logIn()
+    {
+        driver.FindElement(By.CssSelector("li .AccountIcon div:nth-child(1)")).Click();
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult = wait.Until(e => e.FindElement(By.CssSelector(".AuthPage-segmentedControlBtn:nth-child(3)")));
+        driver.FindElement(By.CssSelector(".AuthPage-segmentedControlBtn:nth-child(3)")).Click();
+        WebDriverWait wait1 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult1 = wait.Until(e => e.FindElement(By.CssSelector(".AuthForms-formInput:nth-child(1)")));
+        driver.FindElement(By.CssSelector(".AuthForms-formInput:nth-child(1)")).SendKeys("oliver.werthen@hotmail.com");
+        driver.FindElement(By.CssSelector(".AuthForms-formInput:nth-child(2)")).SendKeys("TestTest123");
+        driver.FindElement(By.CssSelector(".AuthForms-submitBtn")).Click();
+        WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        IWebElement firstResult2 = wait.Until(e => e.FindElement(By.CssSelector(".AccountPage-linkButton")));
+        var elements = driver.FindElements(By.CssSelector(".AccountEmptyState-img"));
+        Assert.True(elements.Count > 0);
+    }
 }
+
+
